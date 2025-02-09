d6fd74f0c5c4895fcbe18df3d8150ec7f9e36beb
Barradas100Pai <gui1.barradas@gmail.com>
2025-02-09 00:12:15 +0000

    brah

--- scripts/Dialogue.cs
+++ scripts/Dialogue.cs
@@ -9,17 +9,26 @@ public partial class Dialogue : Node2D
 	private Texture2D LeftCharacterSprite2D { get; set; }
 	
 	[Export]
-	private Texture2D RightCharacterSprite2D { get; set; }	
+	private Texture2D RightCharacterSprite2D { get; set; }
 	
 	//ADICIONAR RESOURCE DE DIÁLOGO
 	//ainda não criei a classe
+
+	public enum DialogueState
+	{
+		Idle,
+		Talking,
+		Talked,
+		Finished,
+	}
 	
+	private DialogueState state = DialogueState.Idle;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		//AQUI VOU TER QUE PROGRAMAR AS ANIMAÇÕES DAS PERSONAGENS PROVAVELMENTE
-		var leftCharacter = this.GetNode<Sprite2D>("LeftCharacter");
-		var rightCharacter = this.GetNode<Sprite2D>("RightCharacter");
+		var leftCharacter = GetNode<Sprite2D>("LeftCharacter");
+		var rightCharacter = GetNode<Sprite2D>("RightCharacter");
 		leftCharacter.Texture = LeftCharacterSprite2D;
 		rightCharacter.Texture = RightCharacterSprite2D;
 		//ADICIONAR AQUI A CONFIGURAÇÃO DE DIÁLOGOS MAIS TARDE
